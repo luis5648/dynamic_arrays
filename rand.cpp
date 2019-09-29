2484d92805975dbe75455525005e7489e64f3020
Luis Roman <porta5648@gmail.com>
2019-09-28 23:18:43 -0500

    separado funcional

--- rand.cpp
+++ rand.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
-
+#define ARR_SIZE 10
 using namespace std;
 
 //globals
-int random1 = rand() % 10 + 0;
+int random1; 
+
+//prototypes
+void stdlibRandom();
+void algorithmRandom();
 
 int main(){
-	printf("%d",random1);
-
 
 	cin.get();
+
 	return 0;
+}
+
+void stdlibRandom(){
+	
+	for (int i = 0; i < 10; i++){
+
+		random1 = rand() % 10 + 0;
+
+		printf("%d\n",random1);
+	}
+}
+
+void algorithmRandom(){
+
 }

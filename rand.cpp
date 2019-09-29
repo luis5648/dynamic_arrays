c5c875e4cb8e3100fe207c62f4af904e18815c00
Luis Roman <porta5648@gmail.com>
2019-09-28 22:52:48 -0500

    programa random

---
+++ rand.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+//globals
+int random1 = rand() % 10 + 0;
+
+int main(){
+	printf("%d",random1);
+
+
+	cin.get();
+	return 0;
+}
